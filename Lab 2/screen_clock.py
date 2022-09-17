9d4a7154a1f6222476347dbc084c3190e53d3b92
Matthew Cline <clinemattw@gmail.com>
2022-09-16 20:57:20 -0400

    part e

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,4 +1,7 @@
 import time
+import datetime
+import random
+from util import scale_image
 import subprocess
 import digitalio
 import board
@@ -59,17 +62,98 @@ font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
 backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+# filenames of time markers in order
+time_markers = ["good_morning.jpg", "bath_time.jpg", "sun.jpg", "lunch.jpg",
+                "afternoon_nap.jpg", "post_afternoon_nap.jpg", 
+                "pre_walk_excitement.JPG", "walk.jpg", 
+                "snack_request_before_bed.JPG", "bed.jpg"]
+
+cur_index = 0
+manual_viewing = False
 
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    cmd = f"echo '{time.strftime('%m/%d/%Y %H:%M:%S')}'" 
-    TIME = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    # Part D
+    # cmd = f"echo '{time.strftime('%m/%d/%Y %H:%M:%S')}'" 
+    # TIME = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
-    y = top
-    draw.text((x, y), TIME, font=font, fill="#FFFFFF")
+    # y = top
+    # draw.text((x, y), TIME, font=font, fill="#FFFFFF")
 
-    # Display image.
-    disp.image(image, rotation)
-    time.sleep(1)
+    # Part E
+    if buttonB.value and not buttonA.value:  # just button A pressed
+        print("prev")
+
+        if not manual_viewing:  # start at the beginning
+            cur_index = 0
+            manual_viewing = True
+        elif cur_index > 0:
+            cur_index -= 1 
+        else:
+            cur_index = len(time_markers) - 1
+        # Display image.
+        image = Image.open(time_markers[cur_index])
+        disp.image(scale_image(image, height, width))
+    if buttonA.value and not buttonB.value:  # just button B pressed
+        print("next")
+        
+        if not manual_viewing:  # start at the beginning
+            cur_index = 0
+            manual_viewing = True
+        elif cur_index < len(time_markers) - 1:
+            cur_index += 1
+        else:
+            cur_index = 0
+
+        # Display image.
+        image = Image.open(time_markers[cur_index])
+        disp.image(scale_image(image, height, width))
+    
+    elif not buttonA.value and not buttonB.value:   # both pressed
+        # go back to scheduled images
+        manual_viewing = False
+
+    if not manual_viewing:
+        now = datetime.datetime.now()
+        wake_up = now.replace(hour=8, minute=0, second=0, microsecond=0)
+        bath_time = now.replace(hour=10, minute=0, second=0, microsecond=0)
+        sun = now.replace(hour=10, minute=30, second=0, microsecond=0)
+        lunch = now.replace(hour=12, minute=0, second=0, microsecond=0)
+        afternoon_nap = now.replace(hour=13, minute=0, second=0, microsecond=0)
+        post_afternoon_nap = now.replace(hour=15, minute=0, second=0, microsecond=0)
+        pre_walk_excitement = now.replace(hour=16, minute=00, second=0, microsecond=0)
+        walk = now.replace(hour=16, minute=15, second=0, microsecond=0)
+        snack_request_before_bed = now.replace(hour=19, minute=30, second=0, microsecond=0)
+        bed_time = now.replace(hour=20, minute=0, second=0, microsecond=0)
+
+        if wake_up < now < bath_time:
+            image = Image.open("good_morning.jpg")
+        elif bath_time < now < sun:
+            image = Image.open("bath_time.jpg")
+        elif sun < now < lunch:
+            image = Image.open("sun.jpg")
+        elif lunch < now < afternoon_nap:
+            image = Image.open("lunch.jpg")
+        elif afternoon_nap < now < post_afternoon_nap:
+            image = Image.open("afternoon_nap.jpg")
+        elif post_afternoon_nap < now < pre_walk_excitement:
+            image = Image.open("post_afternoon_nap.jpg")
+        elif pre_walk_excitement < now < walk:
+            image = Image.open("pre_walk_excitement.JPG")
+        elif walk < now < snack_request_before_bed:
+            image = Image.open("walk.jpg")
+        elif snack_request_before_bed < now < bed_time:
+            image = Image.open("snack_request_before_bed.jpg")
+        else: # bed time
+            image = Image.open("bed.jpg")
+
+        # Display image.
+        disp.image(scale_image(image, height, width))
+        time.sleep(1)
